13561cfb81919f7b939fac997bae569f4fd6b3a7
Pierfrancesco Calviello <pcalviello@apuliadigitalmaker.it>
2024-04-15 13:34:22 +0200

    ultima modifica

--- index.js
+++ index.js
@@ -1,7 +1,40 @@
 console.log("Ciao da VSC");
- let nome = "Pierfrancesco";
- let cognome = "Calviello";
+let nome = "Pierfrancesco";
+let cognome = "Calviello";
 
-//  JQUERY
- src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"
- $("document").ready( funciton)
+$(document).ready(function () {
+
+    $("#btn_cod_fiscale").on("click", function(){
+        alert($("#test3").val("Ciao"));
+        console.log("CIAO");
+        
+    });
+   
+    $("#title1").on("click", function(){
+    alert("Complimenti");
+    console.log("stampa");
+   });
+   
+    //Jquery
+    $("#btn_mostra_username").on("click", function () {
+        //alert("stai per visualizzare l'username");
+        let username = "tuoNome";
+        $("#username").text(username)
+        .css("font-weight", "bold");
+    }
+    );
+    //Javascript
+    document.getElementById("btn_cambia_testo").addEventListener("click", function () {
+        let nome = "Piero";
+        //alert("Ciao " + nome);
+        console.log("Benvenuto nella funzione 'Callback'");
+        //    Utilizziamo JQUERY che dopo l'alert cambierà il contenuto del p con id="password"
+        $("#password").text("its2029")
+            .css("color", "blue");
+        $(".bordino").css("border", "1px solid red")
+            .css("background-color", "green");
+
+    })
+    console.log("ready!");
+
+});
